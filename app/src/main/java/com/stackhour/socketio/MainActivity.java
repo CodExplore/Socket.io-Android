ad795e9e539d31dc2846308c89029090754e88d7
unknown <azonranker@yahoo.com>
2017-04-27 23:22:40 +0530

    socket.io

--- app/src/main/java/com/stackhour/socketio/MainActivity.java
+++ app/src/main/java/com/stackhour/socketio/MainActivity.java
@@ -4,12 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+	
+	private Socket connectSocket {
+		
+		try{
+			connectSocket = IO.socket("");
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
+       socket.connect();
     }
+	
+	
 }
